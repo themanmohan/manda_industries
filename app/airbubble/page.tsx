076c7ba3ac53f840e19eb8a0f20f7daea76f4d7f
themanmohan <gitmail223344@gmail.com>
2024-10-14 22:06:06 +0530

    refactor: removing comments

--- app/airbubble/page.tsx
+++ app/airbubble/page.tsx
@@ -15,7 +15,7 @@ function Airbubble() {
 
     const responsive = {
         superLargeDesktop: {
-            // the naming can be any, depends on you.
+      
             breakpoint: { max: 4000, min: 3000 },
             items: 5
         },
