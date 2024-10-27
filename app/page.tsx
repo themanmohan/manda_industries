a58aa420b233fc814aa533ae0ea9af7efb51a155
themanmohan <gitmail223344@gmail.com>
2024-10-27 16:19:42 +0530

    fix: fixing build issue second approach

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React,{useState, useEffect} from "react"
-import Image, {StaticImageData} from "next/image";
+import Image from "next/image";
 
 import 'react-multi-carousel/lib/styles.css';
 import { DownloadIcon } from "@/assets/svgIcon";
