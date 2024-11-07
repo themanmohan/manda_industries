8f2cd7e4f4914fdbd2fb671f5c5026b61ca0d0a0
themanmohan <gitmail223344@gmail.com>
2024-11-07 13:18:37 +0530

    style/fix: fixing animation issue on home page

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import React,{useEffect} from 'react'
-import AOS from "aos"
+import React, { useEffect, useState } from 'react';
+import AOS from "aos";
 import toast from 'react-hot-toast';
 
 import { HeroBanner } from "@/components";
@@ -11,6 +11,7 @@ import ContactDetailCard from "@/components/contact-page/ContactDetailCard";
 const notify = () => toast('Your response has been successfully submitted.');
 
 const Contact = () => {
+  const [isMounted, setIsMounted] = useState(false);
 
   const submitFormHandler = () => {
     notify();
@@ -19,35 +20,32 @@ const Contact = () => {
 
   useEffect(() => {
     AOS.init({});
+    setIsMounted(true);
   }, []);
 
+  if (!isMounted) return null;
+
   return (
-      <section className="mb-14" >
-    
+      <section className="mb-14">
         <HeroBanner lableText="Contact Us" />
-        <h2 className="text-center text-3xl font-krona mt-10 mb-8 ">Contact Details</h2>
+        <h2 className="text-center text-3xl font-krona mt-10 mb-8">Contact Details</h2>
 
-        <div className="max-container xl:flex md:block w-full gap-10 whitespace" data-aos="fade-right">
-     
+        <div className="max-container xl:flex md:block w-full gap-10 whitespace" data-aos="fade-left">
           <div className="xl:w-1/2 md:w-full flex flex-col justify-center">
-
             {
-              officeLocations.map((officeLocation,i)=>{
+              officeLocations.map((officeLocation, i) => {
                 return <ContactDetailCard
-                key={i}
-                title={officeLocation.title}
-                address={officeLocation.address}
-                mobileNumber={officeLocation.mobilenumber}
-                emailAddress={officeLocation.emailAddress}
-              />
+                  key={i}
+                  title={officeLocation.title}
+                  address={officeLocation.address}
+                  mobileNumber={officeLocation.mobilenumber}
+                  emailAddress={officeLocation.emailAddress}
+                />
               })
             }
-
-
           </div>
 
-          <div className="xl:w-1/2 md:w-full xl:mt-0 mt-5 bg-lightBlue p-5 md:p-10 rounded-md "  data-aos="fade-left">
-
+          <div className="xl:w-1/2 md:w-full xl:mt-0 mt-5 bg-lightBlue p-5 md:p-10 rounded-md" data-aos="fade-right">
             <div className="relative mt-4 mb-14">
               <select
                 className="peer border-b-2 border-gray-300 focus:outline-none focus:border-blue-500 placeholder-transparent"
@@ -58,7 +56,7 @@ const Contact = () => {
                 <option value="option2">EPE</option>
               </select>
               <label
-                className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-black peer-focus:-top-4.5 peer-focus:text-sm  pl-2"
+                className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-placeholder-shown:text-black peer-focus:-top-4.5 peer-focus:text-sm pl-2"
                 htmlFor="select"
               >
                 Select product
@@ -74,14 +72,14 @@ const Contact = () => {
                   id="name"
                 />
                 <label
-                  className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-4.5 peer-focus:text-sm  pl-2"
+                  className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-4.5 peer-focus:text-sm pl-2"
                   htmlFor="name"
                 >
                   Name
                 </label>
               </div>
 
-              <div className="relative w-full xl:mb-0 ">
+              <div className="relative w-full xl:mb-0">
                 <input
                   className="peer border-b-2 border-gray-300 focus:outline-none focus:border-blue-500 placeholder-transparent"
                   type="text"
@@ -89,13 +87,12 @@ const Contact = () => {
                   id="company"
                 />
                 <label
-                  className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base  peer-focus:-top-4.5 peer-focus:text-sm  pl-2"
+                  className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-4.5 peer-focus:text-sm pl-2"
                   htmlFor="company"
                 >
                   Company Name
                 </label>
               </div>
-
             </div>
 
             <div className="xl:flex sm:block gap-5 w-full mb-14">
@@ -107,7 +104,7 @@ const Contact = () => {
                   id="number"
                 />
                 <label
-                  className="mb-2 absolute left-0 -top-3.5 text-gray-600  text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base  peer-focus:-top-4.5 peer-focus:text-sm  pl-2"
+                  className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-4.5 peer-focus:text-sm pl-2"
                   htmlFor="number"
                 >
                   Contact Number
@@ -122,31 +119,30 @@ const Contact = () => {
                   id="email"
                 />
                 <label
-                  className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base  peer-focus:-top-4.5 peer-focus:text-sm  pl-2"
+                  className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-4.5 peer-focus:text-sm pl-2"
                   htmlFor="email"
                 >
                   Email
                 </label>
               </div>
-
             </div>
 
             <div className="xl:flex sm:block gap-5 w-full mb-14">
               <div className="relative w-full mb-14 xl:mb-0">
-              <select
-                className="peer border-b-2 border-300 border-gray-300 focus:outline-none focus:border-blue-500 placeholder-transparent"
-                id="state"
-              >
-                <option value=""></option>
-                <option value="option1">Himachal</option>
-                <option value="option2">Punjab</option>
-              </select>
-              <label
-                className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-4.5 peer-focus:text-sm  pl-2"
-                htmlFor="state"
-              >
-                Select State
-              </label>
+                <select
+                  className="peer border-b-2 border-300 border-gray-300 focus:outline-none focus:border-blue-500 placeholder-transparent"
+                  id="state"
+                >
+                  <option value=""></option>
+                  <option value="option1">Himachal</option>
+                  <option value="option2">Punjab</option>
+                </select>
+                <label
+                  className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-4.5 peer-focus:text-sm pl-2"
+                  htmlFor="state"
+                >
+                  Select State
+                </label>
               </div>
 
               <div className="relative w-full">
@@ -157,16 +153,14 @@ const Contact = () => {
                   id="city"
                 />
                 <label
-                  className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-4.5 peer-focus:text-sm  pl-2"
+                  className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-4.5 peer-focus:text-sm pl-2"
                   htmlFor="city"
                 >
                   City/Town/District
                 </label>
               </div>
-
             </div>
 
- 
             <div className="relative mt-4 mb-10">
               <textarea
                 className="peer border-b-2 border-gray-300 focus:outline-none focus:border-blue-500 placeholder-transparent"
@@ -175,7 +169,7 @@ const Contact = () => {
                 id="message"
               ></textarea>
               <label
-                className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-4.5 peer-focus:text-sm  pl-2"
+                className="mb-2 absolute left-0 -top-3.5 text-gray-600 text-sm transition-all peer-placeholder-shown:top-2 peer-placeholder-shown:text-base peer-focus:-top-4.5 peer-focus:text-sm pl-2"
                 htmlFor="message"
               >
                 Enter your message
@@ -183,31 +177,20 @@ const Contact = () => {
             </div>
 
             <p
-onClick={submitFormHandler}
-className={`flex justify-center w-auto items-center gap-2 px-5 py-3 border font-krona text-sm rounded-md leading-none bg-coral-red text-white border-coral-red`}
->
-
-Submit
-
-</p>
-            
+              onClick={submitFormHandler}
+              className={`flex justify-center w-auto items-center gap-2 px-5 py-3 border font-krona text-sm rounded-md leading-none bg-coral-red text-white border-coral-red`}
+            >
+              Submit
+            </p>
           </div>
-
         </div>
 
-       
         <section className='mt-10 max-container whitespace flex justify-center flex-wrap gap-9' data-aos="zoom-in">
           {managementContacts.map((managementContact, i) => (
             <ManagmentContactCard key={i} {...managementContact} />
           ))}
-         </section>
-
-
-
-         
-     
-      </section >
- 
+        </section>
+      </section>
   );
 };
 
